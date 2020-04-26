3c49721f8fcbb0e3dd3aa8b27536e92997c5b03a
DanielePallini <s1083424@studenti.univpm.it>
2020-04-26 16:51:40 +0200

    Bollettaluce

--- app/src/main/java/com/example/login/entities/BollettaLuce.java
+++ app/src/main/java/com/example/login/entities/BollettaLuce.java
@@ -3,21 +3,10 @@ package com.example.login.entities;
 public class BollettaLuce extends Bolletta {
     private double consumo = 0;
     private String misura = "kWh";
-    private int max = 0;
 
-    public int getMax() {
-        return max;
-    }
-
-    public void setMax(int max) {
-        this.max = max;
-    }
-
-    public BollettaLuce(int id, double costo, String dataScadenza, String periodo, String finePeriodo, double consumo, String misura, int max){
+    public BollettaLuce(int id, double costo, String dataScadenza, String periodo, String finePeriodo, double consumo){
         super(id, costo, dataScadenza, periodo, finePeriodo);
         this.consumo = consumo;
-        this.misura = misura;
-        this.max = max;
     }
 
     public void setConsumo(double consumo) {
