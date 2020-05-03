fb6ae8027bec0bea795efa2747ff8173d7e43681
DanielePallini <s1083424@studenti.univpm.it>
2020-05-03 16:33:08 +0200

    Update grafici

--- app/src/main/java/com/example/login/fragments/FragmentGrafici.java
+++ app/src/main/java/com/example/login/fragments/FragmentGrafici.java
@@ -1,23 +1,26 @@
 package com.example.login.fragments;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.login.MainActivity;
 import com.example.login.R;
+import com.example.login.charts.BarChartItem;
 import com.example.login.entities.BollettaLuce;
+import com.example.login.charts.ChartItem;
+import com.example.login.charts.LineChartItem;
+import com.example.login.charts.PieChartItem;
 import com.example.login.uiutilities.FeedAdapter;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -26,28 +29,13 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.List;
-
-import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class FragmentGrafici extends Fragment {
 
@@ -65,26 +53,13 @@ public class FragmentGrafici extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         View view = inflater.inflate(R.layout.fragment_grafici, container, false);
 
         //BarChart chart = view.findViewById(R.id.barchart);
-
+        /*
         LineChart chart = (LineChart) view.findViewById(R.id.chart);
-        mAuth = FirebaseAuth.getInstance();
-        final FirebaseUser currentUser = mAuth.getCurrentUser();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        //scaricaDati(db, currentUser.getUid());
-
-
-
-
-        //entries.add(new Entry(15, 15));
-        //entries.add(new Entry(16, 16));
-
-        //entries.add(new Entry(15,15));
-        //for (BollettaLuce data : bollette) {
-            // turn your data into Entry objects
         ArrayList<BollettaLuce> list= MainActivity.bollette;
         for (BollettaLuce data : list) {
 
@@ -101,58 +76,147 @@ public class FragmentGrafici extends Fragment {
         chart.setData(lineData);
         chart.invalidate(); // refresh
 
+        */
 
+
+
+
+
+        getActivity().setTitle("ListViewMultiChartActivity");
+
+        ListView lv = view.findViewById(R.id.listView1);
+
+        ArrayList<ChartItem> list = new ArrayList<>();
+
+        // 30 items
+        for (int i = 0; i < 30; i++) {
+
+            if(i % 3 == 0) {
+                list.add(new LineChartItem(generateDataLine(i + 1), getActivity().getApplicationContext()));
+            } else if(i % 3 == 1) {
+                list.add(new BarChartItem(generateDataBar(i + 1), getActivity().getApplicationContext()));
+            } else if(i % 3 == 2) {
+                list.add(new PieChartItem(generateDataPie(), getActivity().getApplicationContext()));
+            }
+        }
+
+        ChartDataAdapter cda = new ChartDataAdapter(getActivity().getApplicationContext(), list);
+        lv.setAdapter(cda);
         return view;
     }
 
-    public void scaricaDati(FirebaseFirestore db, String uid) {
-        db.collection("utenti").document(uid).collection("bollette")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+    /** adapter that supports 3 different item types */
+    private class ChartDataAdapter extends ArrayAdapter<ChartItem> {
 
+        ChartDataAdapter(Context context, List<ChartItem> objects) {
+            super(context, 0, objects);
+        }
 
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            //noinspection ConstantConditions
+            return getItem(position).getView(position, convertView, getContext());
+        }
 
+        @Override
+        public int getItemViewType(int position) {
+            // return the views type
+            ChartItem ci = getItem(position);
+            return ci != null ? ci.getItemType() : 0;
+        }
 
-                        if (task.isSuccessful()) {
-
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                //String st = document.getId();
-                                String obj = document.getData().toString();
-                                String[] str = obj.split(",");
-                                String[] Dati = new String[6];
-
-                                for(int i = 0; i < 6; i++){
-                                /*{
-                                    switch(i){
-                                        case 0:
-
-                                    }
-
-                                 */
-                                    String[] str1 = str[i].split("=");
-                                    Dati[i] = str1[1];
-                                    //Log.d(TAG, Dati[i]);
-
-                                }
-                                Dati[5] = Dati[5].substring(0, Dati[5].length() - 1); //metodo agricolo ma efficace
-                                BollettaLuce bollettaLuce = new BollettaLuce(Integer.parseInt(Dati[3]), Double.parseDouble(Dati[5]), Dati[1], Dati[4], Dati[0], Double.parseDouble(Dati[2]));
-                                bollette.add(bollettaLuce);
-                                Entry entry = new Entry(15,15);
-                                entries.add(entry);
-
-
-                            }
-
-
-
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-
-                    }
-                });
-
+        @Override
+        public int getViewTypeCount() {
+            return 3; // we have 3 different item-types
+        }
     }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return Line data
+     */
+    private LineData generateDataLine(int cnt) {
+
+        ArrayList<Entry> values1 = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            values1.add(new Entry(i, (int) (Math.random() * 65) + 40));
+        }
+
+        LineDataSet d1 = new LineDataSet(values1, "New DataSet " + cnt + ", (1)");
+        d1.setLineWidth(2.5f);
+        d1.setCircleRadius(4.5f);
+        d1.setHighLightColor(Color.rgb(244, 117, 117));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> values2 = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            values2.add(new Entry(i, values1.get(i).getY() - 30));
+        }
+
+        LineDataSet d2 = new LineDataSet(values2, "New DataSet " + cnt + ", (2)");
+        d2.setLineWidth(2.5f);
+        d2.setCircleRadius(4.5f);
+        d2.setHighLightColor(Color.rgb(244, 117, 117));
+        d2.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setCircleColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setDrawValues(false);
+
+        ArrayList<ILineDataSet> sets = new ArrayList<>();
+        sets.add(d1);
+        sets.add(d2);
+
+        return new LineData(sets);
+    }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return Bar data
+     */
+    private BarData generateDataBar(int cnt) {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 12; i++) {
+            entries.add(new BarEntry(i, (int) (Math.random() * 70) + 30));
+        }
+
+        BarDataSet d = new BarDataSet(entries, "New DataSet " + cnt);
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        d.setHighLightAlpha(255);
+
+        BarData cd = new BarData(d);
+        cd.setBarWidth(0.9f);
+        return cd;
+    }
+
+    /**
+     * generates a random ChartData object with just one DataSet
+     *
+     * @return Pie data
+     */
+    private PieData generateDataPie() {
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            entries.add(new PieEntry((float) ((Math.random() * 70) + 30), "Quarter " + (i+1)));
+        }
+
+        PieDataSet d = new PieDataSet(entries, "");
+
+        // space between slices
+        d.setSliceSpace(2f);
+        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        return new PieData(d);
+    }
+
+
+
+
 }
